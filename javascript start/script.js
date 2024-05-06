2bf24dabf8031d07985f847cef8b770505fe300e
mariyamsaleem32 <164928689+mariyamsaleem32@users.noreply.github.com>
2024-05-06 16:31:48 +0500

    Add files via upload

---
+++ javascript start/script.js
@@ -0,0 +1,50 @@
+alert("welcome to my website");
+document.write("hello world");
+console.log("a");
+
+let first_name;           //decleration
+let last_name;            
+first_name ="maryam";    //initialization
+last_name ="saleem";    
+console.log(first_name);
+console.log(last_name);
+
+ let a = 30;
+ let b = 30;
+ let c = a+b;
+ console.log("your result is :" +(a+b));  //substraction
+
+ var birthDate = 2006;
+ var myName    = "mariyam";
+ var email     = "mariyamsaleeem45@gmail.com";
+ console.log(myName +" " + birthDate + " " + email); //concatinate means using +
+
+ let x = 50;
+ let y = 30;
+
+ console.log("add two numbers:" + (x+y));
+ console.log("minas two numbers:" + (x-y));
+ console.log("multiply two numbers:" + (x*y));
+ console.log("divide two numbers:" + (x/y));
+ console.log("modulus two numbers:" + (x%y));
+
+// Legal Variable
+let myName = "mariyam";
+let birthDate = 2006;
+let $email  = "mariyamsaleeem45@gmail.com";
+let Age;
+let age20;
+console.log(myName +" " + birthDate + " " + $email); 
+ 
+//Illegal Variable
+let 12;
+let 12return;
+let !hello;
+let @34answer;
+let "Jaweria";
+let ?khan;
+
+let costOfOneTicket = 600;
+let BuyingTickets =5;
+let result = costOfOneTicket * BuyingTickets;
+console.log("total cost of tickets is:"+ result);
